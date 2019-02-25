86691de1b6f47ce1cdf9239568720731ca148f0c
Jas0nxlee <jas0n@live.com>
2019-02-25 16:32:01 +0800

    123

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 '''获取经纬度'''
-import jason
-from urllib.request import uropen,quote
+import json
+from urllib.request import urlopen,quote
 import csv
 import pandas as pd
 import traceback
@@ -10,27 +10,35 @@ os.chdir('E:/git/cheakpoint')
 data=pd.read_csv('in.csv')
 
 def getlnglat(address):
-    url = 'http://api.map.baidu/geocoder/v2/'
+    url = 'http://api.map.baidu.com/geocoder'
     output = 'json'
     ak = '896ea24559ad7895f74b451cffb63bbe'
     add = quote(address)
-    uri = url + '?' + 'address=' + add + '&output=' + output + '&ak=' + a
-    req = urlopen(url)
+    uri = url + '?key=' + ak + '&output=json&address=' + add
+    print(uri)
+    req = urlopen(uri)
     res = req.read().decode()
     temp = json.loads(res)
     return temp
 
 
 file = open('经纬度.json','w')
-with open ('in.csv'.'r') as csvfile:
+with open ('in.csv','r',encoding="utf-8") as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
-        if reader.line.num == 1:
+        if reader.line_num == 1:
             continue
         b = line[0].strip()
         c = str(line[1]).strip()
         try:
             lng = getlnglat(b)['result']['location']['lng']
             lat = getlnglat(b)['result']['location']['lat']
-            str_temp = '{"lat":' + str(lat) + ',"lng":' + str(lng) + ',"count":'}
-
+            str_temp = '{"lat":' + str(lat) + ',"lng":' + str(lng) + ',"count":' + str(c) +'},'
+            file.write(str_temp)
+        except:
+            f=open("异常日志.txt",'a')
+            traceback.print_exc(file=f)
+            f.flush()
+            f.close()
+            print('发生异常')
+file.close()
